c50597e8ea1ab1a887c5daae64403743ee0c300e
OxiD1Zed <skabo1000@mail.ru>
2023-04-06 13:39:14 +0300

    Начальный код

---
+++ ЗачетноеЗадание/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ЗачетноеЗадание
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonRandom = new System.Windows.Forms.Button();
+            this.buttonResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 96);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonRandom
+            // 
+            this.buttonRandom.Location = new System.Drawing.Point(282, 12);
+            this.buttonRandom.Name = "buttonRandom";
+            this.buttonRandom.Size = new System.Drawing.Size(173, 30);
+            this.buttonRandom.TabIndex = 1;
+            this.buttonRandom.Text = "Сгенерировать";
+            this.buttonRandom.UseVisualStyleBackColor = true;
+            this.buttonRandom.Click += new System.EventHandler(this.buttonRandom_Click);
+            // 
+            // buttonResult
+            // 
+            this.buttonResult.Location = new System.Drawing.Point(282, 78);
+            this.buttonResult.Name = "buttonResult";
+            this.buttonResult.Size = new System.Drawing.Size(173, 30);
+            this.buttonResult.TabIndex = 2;
+            this.buttonResult.Text = "Вычислить";
+            this.buttonResult.UseVisualStyleBackColor = true;
+            this.buttonResult.Click += new System.EventHandler(this.buttonResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 120);
+            this.Controls.Add(this.buttonResult);
+            this.Controls.Add(this.buttonRandom);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Массив чисел";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonRandom;
+        private System.Windows.Forms.Button buttonResult;
+    }
+}
+
---
+++ ЗачетноеЗадание/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ЗачетноеЗадание
+{
+    public partial class Form1 : Form
+    {
+        private bool _isResult;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _isResult = false;
+        }
+
+        private void buttonRandom_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Random r = new Random();
+            int n = r.Next(3, 15);
+            for(int i = 0; i < n; i++)
+            {
+                textBox1.Text += r.Next(100) + (i == n - 1 ? "" : " ");
+            }
+        }
+
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            List<int> nums = new List<int>();
+            string num = "";
+            for(int i = 0; i < textBox1.Text.Length; i++)
+            {
+                if (textBox1.Text[i] != ' ')
+                {
+                    num += textBox1.Text[i];
+                    if (i == textBox1.Text.Length - 1)
+                        nums.Add(int.Parse(num));
+                }
+                else
+                {
+                    nums.Add(int.Parse(num));
+                    num = "";
+                }
+            }
+            _isResult = true;
+            textBox1.Text = Math.Round(nums.Sum() / Convert.ToDouble(nums.Count), 5).ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!_isResult)
+            {
+                const string nums = "1234567890 ";
+
+                for (int i = 0, l = 0; i < textBox1.Text.Length - l; i++)
+                {
+                    if (!nums.Contains(textBox1.Text[i]) || (textBox1.Text[i] == ' ' && i < 1) || (i > 0 && textBox1.Text[i] == ' ' && textBox1.Text[i - 1] == ' '))
+                    {
+                        textBox1.Text = textBox1.Text.Remove(i, 1);
+                        --i;
+                        ++l;
+                    }
+                }
+            }
+            else
+            {
+                _isResult = false;
+            }
+        }
+    }
+}
